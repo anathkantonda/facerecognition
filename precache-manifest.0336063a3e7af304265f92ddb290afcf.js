5eac3789a8817f183b2e37f11bde218d66771a2c
Anath <anathkantonda@gmail.com>
2020-05-05 15:00:40 +0200

    Updates

---
+++ precache-manifest.0336063a3e7af304265f92ddb290afcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb299941876344e5065059ac185c292",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "78ca2a9b13e2b153c835",
+    "url": "/facerecognition/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "6d152e8eaf08027f8746",
+    "url": "/facerecognition/static/css/main.b558835d.chunk.css"
+  },
+  {
+    "revision": "78ca2a9b13e2b153c835",
+    "url": "/facerecognition/static/js/2.dcc5fa23.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/facerecognition/static/js/2.dcc5fa23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d152e8eaf08027f8746",
+    "url": "/facerecognition/static/js/main.091ddb4a.chunk.js"
+  },
+  {
+    "revision": "e22a4a638a1b5317a9ac",
+    "url": "/facerecognition/static/js/runtime-main.fc136178.js"
+  }
+]);
